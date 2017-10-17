225b609bf8d6198b6e37838fdeef64e8beba53d2
mauridb <mauriziocontatto@gmail.com>
2017-10-17 19:29:11 +0200

    set logic of the .py script, asap :D

---
+++ Python/knowledge.py
@@ -0,0 +1,13 @@
+# ask learnt thing 
+# ask area detail 
+# ask description
+
+# something like django.contrib.auth.models | python | description 
+
+# read a file that store everything inside the file  with a line separator and a row number... a little db, keep it simple, it helps biggerproject platform that we will dev asap!
+
+# try except something, please check it a good logic please! 
+
+# close file 
+
+
